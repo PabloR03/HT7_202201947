0d3197857f946b130c475f1bd3f56b9ebffef7ca
Pablo Andres Rodriguez Lima <111318588+PabloR03@users.noreply.github.com>
2022-10-03 10:36:59 -0600

    Add files via upload

---
+++ [IPC]_HT7_202201947/HT7txt/src/ht7txt/HT7txt.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ht7txt;
+
+/**
+ *
+ * @author Pablo Rodriguez
+ */
+public class HT7txt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
